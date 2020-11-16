c71c147bf7f1caed5617dafc441f0340cde3a34b
Rodolpho Agizzio Molina <Rodolpho.Agizzio.Molina1@ibm.com>
2020-11-16 08:47:53 -0300

    November 16th update

--- advanced/generator_example.py
+++ advanced/generator_example.py
@@ -7,16 +7,37 @@ def numberGenerator(n):
     finally:
         yield n
 
-myGenerator = numberGenerator(3)
-print(next(myGenerator))
-print(next(myGenerator))
-print(next(myGenerator))
+#myGenerator = numberGenerator(3)
+#print(next(myGenerator))
+#print(next(myGenerator))
+#print(next(myGenerator))
 
-g = numberGenerator(10)
-counter = 0
-while counter < 10:
-    print(next(g))
-    counter += 1
+#g = numberGenerator(10)
+#counter = 0
+#while counter < 10:
+    #print(next(g))
+#    counter += 1
+
+#h = numberGenerator(10)
+#next(h)
+#print(h.send(5))
 
 #newGenerator = numberGenerator(23)
 #print(next(newGenerator))
+
+def myGenerator1(n):
+    for i in range(n):
+        yield i
+
+def myGenerator2(n, m):
+    for j in range(n, m):
+       yield j
+
+def myGenerator3(n, m):
+    yield from myGenerator1(n)
+    yield from myGenerator2(n, m)
+    yield from myGenerator2(n, m + 5)
+
+print(list(myGenerator1(5)))
+print(list(myGenerator2(5, 10)))
+print(list(myGenerator3(0, 10)))
